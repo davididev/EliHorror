81d0512da94aa994e0bd63d1283e34a99810f91f
davididev <davidwaggener@gmail.com>
2024-12-30 15:45:09 -0500

    Got knife and flashlight to snap into place
    Next up is to make the demon model and activate it.

---
+++ Mine/Scripts/Ch1/knife_pickup.gd
@@ -0,0 +1,23 @@
+extends Node3D
+
+@export var Demon_Ref : NodePath;
+@export var dialogue_on_grab : DialogueGrid
+var initial_pickup = false;
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	initial_pickup = false;
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+
+func _on_pickable_object_picked_up(pickable: Variant) -> void:
+	if initial_pickup == true:
+		return;
+	initial_pickup = true;
+	DialogueHandler.Instance.StartDialogue(dialogue_on_grab);
+	await get_tree().create_timer(0.25).timeout;
+	PlayMusic.PlaySong("LetsPlay.mp3");
