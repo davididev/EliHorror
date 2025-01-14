f0b1327dd6c3761e6040007a1d2efb40dfbc112a
davididev <davidwaggener@gmail.com>
2025-01-13 22:32:54 -0500

    Raycast not fully working, stopping point
    It hits some walls but not the enemies

--- Mine/Scripts/Ch3/Demon_Slayer_Gun.gd
+++ Mine/Scripts/Ch3/Demon_Slayer_Gun.gd
@@ -13,7 +13,7 @@ var currentFireFresnel = 0.0;
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	get_node(path_of_gun_fire).visible = false;
+	#get_node(path_of_gun_fire).visible = false;
 	pass # Replace with function body.
 
 
@@ -25,34 +25,35 @@ func _process(delta: float) -> void:
 	
 	var perc = currentChargeTimer / CHARGE_TIME;
 	var vs = get_node(path_of_glow).get_surface_override_material(0) as ShaderMaterial;
-	vs.set_shader_parameter("shader_parameter/PowerValue", lerp(START_GLOW_STRENGTH, END_GLOW_STRENGTH, perc));
+	vs.set_shader_parameter("PowerValue", lerp(START_GLOW_STRENGTH, END_GLOW_STRENGTH, perc));
 	if get_node(path_of_gun_fire).visible == true:
-		currentFireFresnel += 10.0 * delta;
-		if currentFireFresnel > START_GLOW_STRENGTH:
+		currentFireFresnel += 5.0 * delta;
+		if currentFireFresnel >= 15.0:
 			get_node(path_of_gun_fire).visible = false;
 		else:
 			var vs2 = get_node(path_of_gun_fire).get_surface_override_material(0) as ShaderMaterial;
-			vs2.set_shader_parameter("shader_parameter/FloatParameter", lerp(START_GLOW_STRENGTH, END_GLOW_STRENGTH, perc));
+			vs2.set_shader_parameter("FloatParameter", currentFireFresnel);
 
 func _on_pickable_object_action_pressed(pickable: Variant) -> void:
 	if is_equal_approx(currentChargeTimer, CHARGE_TIME):  #Gun has finished charging, create a 
-		var space_state = get_world_3d().direct_space_state
+		currentChargeTimer = 0.0;
+		var space_state = get_world_3d().direct_space_state;
 		
 		var origin = get_node(path_of_gun_fire).global_position;
-		var end = origin + (get_node(path_of_gun_fire).global_basis.z * 100.0)
+		var end = origin + (get_node(path_of_gun_fire).global_basis.z * -100.0)
 		var query = PhysicsRayQueryParameters3D.create(origin, end)
-		query.collide_with_areas = true
-		query.layer_mask = pow(2, 1-1) + pow(2, 6-1); #Static and enemy layer
-		SoundFXPlayer.PlaySound(FireSound, get_tree(), origin, 5.0, 2.0);
+		query.collision_mask = pow(2, 1-1) + pow(2, 6-1); #Static and enemy layer
 		var result = space_state.intersect_ray(query)
-		if result != null:
+		if result.is_empty() == false:
 			#end = result.position;
-			var newScale = Vector3(1.0, 1.0, 1.0);
+			
+			SoundFXPlayer.PlaySound(FireSound, get_tree(), origin, 5.0, 2.0);
 			var s = origin.distance_to(result.position)
-			newScale.y = s;
+			get_node(path_of_gun_fire).scale = Vector3(1.0, s, 1.0);
+			print("Pos: ", result.position, "s: ", s);
 			get_node(path_of_gun_fire).visible = true;
-			get_node(path_of_gun_fire).scale = newScale;
-			currentFireFresnel = END_GLOW_STRENGTH;
+			currentFireFresnel = 1.0;
+			print(result.collider.name);
 			if result.collider != null:
 				if result.collider.has_signal("OnDamage"):
 					result.collider.emit_signal("OnDamage", DAMAGE);
