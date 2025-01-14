52a95ab02a31b8b461525c87229253435a1c3385
davididev <davidwaggener@gmail.com>
2025-01-14 12:11:48 -0500

    Got the gun working
    Turns out I was using the wrong object's basis.
    I also lowered the demon's attack power

--- Mine/Scripts/Ch3/Demon_Slayer_Gun.gd
+++ Mine/Scripts/Ch3/Demon_Slayer_Gun.gd
@@ -40,7 +40,7 @@ func _on_pickable_object_action_pressed(pickable: Variant) -> void:
 		var space_state = get_world_3d().direct_space_state;
 		
 		var origin = get_node(path_of_gun_fire).global_position;
-		var end = origin + (get_node(path_of_gun_fire).global_basis.z * -100.0)
+		var end = origin + (get_node("PickableObject").global_basis.z * -100.0)
 		var lm = pow(2, 1-1) + pow(2, 6-1); #Static and enemy layer
 		var query = PhysicsRayQueryParameters3D.create(origin, end, lm)
 		query.collide_with_bodies = true;
@@ -50,12 +50,12 @@ func _on_pickable_object_action_pressed(pickable: Variant) -> void:
 			#end = result.position;
 			
 			SoundFXPlayer.PlaySound(FireSound, get_tree(), origin, 5.0, 2.0);
-			var s = origin.distance_to(result.position)
+			var s = origin.distance_to(to_global(result.position))
 			get_node(path_of_gun_fire).scale = Vector3(1.0, s, 1.0);
-			print("Pos: ", result.position, "s: ", s);
+			print("Pos: ", to_global(result.position), "s: ", s);
 			get_node(path_of_gun_fire).visible = true;
 			currentFireFresnel = 1.0;
 			print(result.collider.name);
 			if result.collider != null:
 				if result.collider.has_signal("OnDamage"):
-					result.collider.emit_signal("OnDamage", DAMAGE);
+					result.collider.emit_signal("OnDamage", DAMAGE, to_global(result.position));
