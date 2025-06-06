aeda8a234297e95242b50a02c648be18d4780544
davididev <davidwaggener@gmail.com>
2025-06-05 21:25:09 -0400

    Changed calibration code

--- VR/Scripts/Misc/pos_vel_calc.gd
+++ VR/Scripts/Misc/pos_vel_calc.gd
@@ -1,7 +1,5 @@
 class_name PosVelCalc extends Node3D
 
-var damageTimer = -1.0;
-const DAMAGE_DELAY = 0.5;
 static var LeftHandPos : Vector3;
 static var RightHandPos : Vector3;
 static var HeadPos : Vector3;
@@ -12,25 +10,30 @@ static var HeadVel : Vector3;
 var _last_left_hand_position : Vector3;
 var _last_right_hand_position : Vector3;
 var _last_head_position : Vector3;
+#request_jump
 
-
-var blood_hit_prefab : PackedScene = preload("res://Mine/Prefabs/Enemy/blood_spatter_1.tscn");
 @export var LeftHandRef : NodePath;
 @export var RightHandRef : NodePath;
-@export var PlayerbodyRef : NodePath;
 @export var HeadRef : NodePath;
+@export var BodyRef : NodePath;
 
+var _controller1;
+var _controller2;
+
+var body : XRToolsPlayerBody
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	XRToolsUserSettings.snap_turning = false;  #For this game, we don't want snap turning.
-	damage_color_alpha = 0.0;
-	damage_color_alpha_target = 0.0;
-	damageRoutine = false;
-	get_node(PlayerbodyRef).calibrate_player_height();
-
+	body = get_node(BodyRef) as XRToolsPlayerBody;
+	_controller1 = XRHelpers.get_xr_controller(get_node(LeftHandRef))
+	_controller2 = XRHelpers.get_xr_controller(get_node(RightHandRef))
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	if _controller1.get_is_active() and _controller2.get_is_active():
+		if _controller1.get_float("trigger") > 0.9 and _controller1.get_float("grip") < 0.5:
+			if _controller2.get_float("trigger") > 0.9 and _controller2.get_float("grip") < 0.5:
+				body.calibrate_player_height()
+	
 	LeftHandPos = get_node(LeftHandRef).global_position;
 	RightHandPos  = get_node(RightHandRef).global_position;
 	HeadPos = get_node(HeadRef).global_position;
@@ -38,34 +41,7 @@ func _process(delta: float) -> void:
 	RightHandVel = abs((_last_right_hand_position - get_node(RightHandRef).position)) / delta;
 	HeadVel = abs((_last_head_position - get_node(HeadRef).position)) / delta;
 	
+	
 	_last_left_hand_position = get_node(LeftHandRef).position;
 	_last_right_hand_position = get_node(RightHandRef).position
 	_last_head_position = get_node(HeadRef).position;
-	if damageTimer > 0.0:
-		damageTimer -= delta;
-
-	if damageRoutine == true:
-		damage_color_alpha = move_toward(damage_color_alpha, damage_color_alpha_target, 4.0 * delta);
-		if is_equal_approx(damage_color_alpha, 1.0):
-			damage_color_alpha_target = 0.0;
-		if is_equal_approx(damage_color_alpha, 0.0):
-			damageRoutine = false;
-		DialogueHandler.FadeColor = Color(1.0, 0.0, 0.0, damage_color_alpha);
-	
-
-var damage_color_alpha = 0.0;
-var damage_color_alpha_target = 0.0;
-var damageRoutine = false;
-
-func _on_player_body_on_damage(amt: int, hitPos: Vector3) -> void:
-	if damageTimer > 0.0:
-		return;
-	SceneVars.CurrentHealth -= amt;
-	damageTimer = DAMAGE_DELAY;
-	var inst = blood_hit_prefab.instantiate();
-	get_tree().root.add_child(inst);
-	inst.global_position = hitPos;
-	inst.global_basis.z = get_node(HeadRef).global_basis.z;
-	damageRoutine = true;
-	damage_color_alpha_target = 1.0;
-	
